d70f7626dc79cf15633176d3e6c65e3c855a6fb6
Iwan Firmawan <firmawaneiwan@gmail.com>
2025-04-09 16:58:14 +0700

    [#11] Refactor administration seeder
    Remove hard-coded geo_config to set administration_levels and remove unnecessary funcs

--- backend/api/v1/v1_profile/management/commands/administration_seeder.py
+++ backend/api/v1/v1_profile/management/commands/administration_seeder.py
@@ -1,160 +1,109 @@
-import numpy as np
 import pandas as pd
 from django.core.management import BaseCommand
 from api.v1.v1_profile.models import Levels, Administration
 
-geo_config = [
-    {"id": 1, "level": 0, "name": "NAME_0", "alias": "National"},
-    {"id": 2, "level": 1, "name": "NAME_1", "alias": "County"},
-    {"id": 3, "level": 2, "name": "NAME_2", "alias": "Sub-County"},
-    {"id": 4, "level": 3, "name": "NAME_3", "alias": "Ward"},
-    {"id": 5, "level": 4, "name": "NAME_4", "alias": "Village"},
-]
 
-SOURCE_FILE = "./source/fiji-2024.csv"
-
-MAX_LEVEL_IN_SOURCE_FILE = 4
-
-
-def get_parent_id(df, x):
-    if x["level"] == 0:
-        return None
-    parent_level = x["level"] - 1
-    pid = df[(df["level"] == parent_level) & (df["name"] == x["p"])]
-    pid = pid.to_dict("records")[0]
-    return pid["id"]
-
-
-def seed_levels():
+def seed_levels(geo_config: list = []):
     for geo in geo_config:
         level = Levels(id=geo["id"], name=geo["alias"], level=geo["level"])
         level.save()
 
 
-def seed_administration_test(county: list = []):
-    if len(county) == 0:
-        county = [
-            ["Jakarta", "East Jakarta", "Kramat Jati", "Cawang"],
-            ["Yogyakarta", "Sleman", "Seturan", "Cepit Baru"],
-        ]
-    seed_levels()
-    level = Levels.objects.filter(level=0).first()
-    administration = Administration(
-        id=1, name="Indonesia", parent=None, level=level
-    )
-    administration.save()
-    for sx, subcounties in enumerate(county):
-        last_adm = Administration.objects.order_by("-id").first()
-        id = last_adm.id + 1 if last_adm else sx * 10
-        county_name = subcounties[0]
-        county_level = Levels.objects.filter(name="County").first()
-        county = Administration.objects.filter(name=county_name).first()
-        if not county:
-            county = Administration(
-                id=id,
-                name=county_name,
-                parent=administration,
-                level=county_level,
-                path=f"{administration.id}.",
-            )
-            county.save()
-        adm = county
-        for index, name in enumerate(subcounties[1:]):
-            last_adm = Administration.objects.order_by("-id").first()
-            child_id = last_adm.id + 1 if last_adm else id * 3
-            level = Levels.objects.filter(level=index + 2).first()
-            path = "{0}{1}.".format(adm.path, adm.id)
-            adm = Administration(
-                id=child_id, name=name, parent=adm, level=level, path=path
-            )
-            adm.save()
+def seed_administration(row: dict, geo_config: list = []):
+    for geo in geo_config:
+        col_level = f"{geo['level']}_{geo['alias']}"
+        parent = None
+        if geo["level"] > 0:
+            code_parent = row.get(f"{geo['level'] - 1}_code")
+            parent = Administration.objects.filter(
+                code=code_parent, level__level=geo["level"] - 1
+            ).first()
+        # Get the level from the geo_config
+        level = Levels.objects.filter(level=geo["level"]).first()
+        # Get the code from the row
+        code = row.get(f"{geo['level']}_code")
+        # Get the name from the row
+        name = row[col_level]
+
+        Administration.objects.update_or_create(
+            name=name,
+            defaults={
+                "level": level,
+                "code": code,
+                "parent": parent,
+            },
+        )
 
 
-def get_path(df, parent, current=[]):
-    p = df[df["id"] == parent].reset_index()
-    if p.shape[0]:
-        current = current + [p["id"][0]]
-        return get_path(df, list(p["parent"])[0], current)
-    current.reverse()
-    path = ".".join([str(c) for c in current])
-    if len(path):
-        return f"{path}."
-    return None
-
-
-def seed_administration_prod():
-    Administration.objects.all().delete()
-    Levels.objects.all().delete()
-    seed_levels()
-    levels = [
-        c["alias"]
-        for c in geo_config
-        if c["level"] <= MAX_LEVEL_IN_SOURCE_FILE
+def seed_administration_test():
+    geo_config = [
+        {"id": 1, "level": 0, "name": "NAME_0", "alias": "National"},
+        {"id": 2, "level": 1, "name": "NAME_1", "alias": "Province"},
+        {"id": 3, "level": 2, "name": "NAME_1", "alias": "District"},
+        {"id": 4, "level": 3, "name": "NAME_2", "alias": "Subdistrict"},
+        {"id": 5, "level": 4, "name": "NAME_3", "alias": "Village"},
     ]
-    properties = pd.read_csv(SOURCE_FILE).to_dict("records")
-    df = pd.DataFrame(properties)
-    # remove duplicates
-    df = df.drop_duplicates()
-    rec = df.to_dict("records")
-    res = []
-    for i, r in enumerate(rec):
-        for iv, v in enumerate(levels):
-            lv = list(filter(lambda x: x["alias"] == v, geo_config))[0][
-                "level"
-            ]
-            parent_id = None
-            if lv > 0:
-                plv = levels[iv - 1]
-                parent_id = r[f"{plv}_code"]
-            id = None
-            if lv < len(levels) - 1:
-                id = r[f"{v}_code"]
-            data = {
-                "ids": id,
-                "name": r[v],
-                "parent": parent_id,
-                "level": lv,
+    seed_levels(geo_config=geo_config)
+    rows = [
+        {
+            "0_code": "ID",
+            "0_National": "Indonesia",
+            "1_code": "ID-JK",
+            "1_Province": "Jakarta",
+            "2_code": "ID-JK-JKE",
+            "2_District": "East Jakarta",
+            "3_code": "ID-JK-JKE-KJ",
+            "3_Subdistrict": "Kramat Jati",
+            "4_code": "ID-JK-JKE-KJ-CW",
+            "4_Village": "Cawang",
+        },
+        {
+            "0_code": "ID",
+            "0_National": "Indonesia",
+            "1_code": "ID-YGK",
+            "1_Province": "Yogyakarta",
+            "2_code": "ID-YGK-SLE",
+            "2_District": "Sleman",
+            "3_code": "ID-YGK-SLE-SET",
+            "3_Subdistrict": "Seturan",
+            "4_code": "ID-YGK-SLE-SET-CEP",
+            "4_Village": "Cepit Baru",
+        },
+    ]
+    for row in rows:
+        seed_administration(row=row, geo_config=geo_config)
+
+
+def seed_administration_prod(
+    source_file: str = "./source/administrations_fiji.csv"
+):
+    Levels.objects.all().delete()
+
+    df = pd.read_csv(source_file)
+    header_columns = df.columns.tolist()
+    geo_config = []
+    for column in header_columns:
+        level = int(column.split("_")[0])
+        alias = column.split("_")[1]
+        if alias.lower() == "code":
+            # Skip the code column
+            continue
+        geo_config.append(
+            {
+                "id": level + 1,
+                "level": level,
+                "name": f"NAME_{level}",
+                "alias": alias,
             }
-            res.append(data)
-    res = pd.DataFrame(res)
-    res = res.dropna(subset=["name"]).reset_index()
-    subset = ["ids", "name", "parent", "level"]
-    res = (
-        res.drop_duplicates(subset=subset)
-        .sort_values(["level", "name"])
-        .reset_index()
-    )
-    res = res[subset]
-    res = (
-        res.sort_values("level")
-        .reset_index()[["ids", "parent", "name", "level"]]
-        .reset_index()
-    )
-    res["id"] = res.apply(
-        lambda x: x["ids"] if x["ids"] == x["ids"] else x["index"] + 50000,
-        axis=1,
-    ).astype(int)
-    res = res[["id", "parent", "name", "level"]]
-    res["path"] = res["parent"].apply(lambda x: get_path(res, x))
-    res = (
-        res.sort_values("id")
-        .reset_index()[["id", "parent", "name", "level"]]
-        .reset_index()
-    )
-    res = res.replace({np.nan: None})
-    for iv, v in enumerate(levels):
-        filtered_res = res[res["level"] == iv]
-        for i, r in enumerate(filtered_res.to_dict("records")):
-            administration = Administration(
-                id=r.get("id"),
-                name=r.get("name").title(),
-                parent=Administration.objects.filter(
-                    id=r.get("parent")
-                ).first(),
-                level=Levels.objects.filter(level=r.get("level")).first(),
-                path=r.get("path"),
-            )
-            administration.save()
+        )
+    geo_config = sorted(geo_config, key=lambda x: x["level"])
+
+    seed_levels(geo_config=geo_config)
+    df = pd.read_csv(source_file)
+    df = df.drop_duplicates()
+    df = df.reset_index(drop=True)
+    for _, row in df.iterrows():
+        seed_administration(row=row, geo_config=geo_config)
 
 
 class Command(BaseCommand):
@@ -165,15 +114,26 @@ class Command(BaseCommand):
         parser.add_argument(
             "-c", "--clean", nargs="?", const=1, default=False, type=int
         )
+        parser.add_argument(
+            "-s",
+            "--source",
+            nargs="?",
+            const=1,
+            default="./source/administrations_fiji.csv",
+            type=str,
+        )
 
     def handle(self, *args, **options):
         test = options.get("test")
         clean = options.get("clean")
+        source_file = options.get("source")
         if clean:
             Administration.objects.all().delete()
             self.stdout.write("-- Administration Cleared")
         if test:
             seed_administration_test()
         if not test:
-            seed_administration_prod()
+            seed_administration_prod(
+                source_file=source_file,
+            )
             self.stdout.write("-- FINISH")
