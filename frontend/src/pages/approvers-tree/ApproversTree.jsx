3ea32692f46d88a9fdac6ee8daa9b75309f78f02
Iwan Firmawan <firmawaneiwan@gmail.com>
2025-04-11 14:01:01 +0700

    [#11] Add maxLevel prop to ApproverFilters and update ApproversTree logic

--- frontend/src/pages/approvers-tree/ApproversTree.jsx
+++ frontend/src/pages/approvers-tree/ApproversTree.jsx
@@ -17,11 +17,12 @@ const ApproversTree = () => {
     selectedForm,
     levels,
   } = store.useState((s) => s);
+  const maxLevel =
+    levels?.length > 2 ? levels?.slice(-2)[0]?.level : levels[0]?.level;
 
   const administration = useMemo(() => {
-    const maxLevel = levels?.slice(-2)[0]?.level;
     return filterOption.filter((item) => item.level !== maxLevel);
-  }, [filterOption, levels]);
+  }, [filterOption, maxLevel]);
 
   const [nodes, setNodes] = useState([]);
   const [dataset, setDataset] = useState([]);
@@ -82,7 +83,8 @@ const ApproversTree = () => {
               {
                 id: selectedAdministration.id,
                 childLevelName:
-                  selectedAdministration.childLevelName || startingLevel?.name,
+                  selectedAdministration.childLevelName ||
+                  selectedAdministration?.children_level_name,
                 children: res.data.map((cI) => ({
                   ...cI,
                   user: cI.user,
@@ -346,6 +348,7 @@ const ApproversTree = () => {
             loading={false}
             disabled={isPristine || loading}
             visible={false}
+            maxLevel={maxLevel}
           />
           <Divider />
           <div style={{ padding: 0, minHeight: "40vh" }}>
