56a6b8e2d0438fc470b2bfb8c4b1427298952a25
Iwan Firmawan <firmawaneiwan@gmail.com>
2025-04-24 16:42:33 +0700

    [#19] Add role and access constants to improve configuration management

---
+++ frontend/src/lib/constants.js
@@ -0,0 +1,5 @@
+export const ROLE_ID_SUPERADMIN = 1;
+export const ROLE_ID_ADMIN = 2;
+export const FORM_ACCESS_ID_READ = 1;
+export const FORM_ACCESS_ID_EDITOR = 2;
+export const FORM_ACCESS_ID_APPROVER = 3;
