58a38065027a19e460105e30de551ce094c5f532
Iwan Firmawan <firmawaneiwan@gmail.com>
2025-04-24 10:56:36 +0700

    [#19] Add test True in demo_approval_flow call

--- backend/api/v1/v1_data/tests/tests_pending_data.py
+++ backend/api/v1/v1_data/tests/tests_pending_data.py
@@ -27,7 +27,7 @@ class PendingDataTestCase(TestCase):
                                          admin_payload,
                                          content_type='application/json')
         self.token = user_response.json().get('token')
-        call_command('demo_approval_flow', "--test")
+        call_command('demo_approval_flow', "--test", True)
 
     def tests_pending_data(self):
         call_command('fake_pending_data_seeder', '-r', 1, '-t', True, '-b', 1)
