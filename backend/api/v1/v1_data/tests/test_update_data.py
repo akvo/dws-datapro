e12babba897ebbdfedceba7270e0a88d2ae71e54
Iwan Firmawan <firmawaneiwan@gmail.com>
2025-04-28 21:56:33 +0700

    [#26] Refactor test cases for update data permissions
    - test Update data by super admin
    - tets Update data by editor

--- backend/api/v1/v1_data/tests/test_update_data.py
+++ backend/api/v1/v1_data/tests/test_update_data.py
@@ -22,12 +22,19 @@ class FormDataUpdateTestCase(TestCase):
             level__level=2
         ).order_by("?").first()
 
-    def test_update_datapoint_by_admin_role(self):
+    def test_update_datapoint_by_superadmin(self):
+        # Login as super admin
         user = {"email": "admin@rush.com", "password": "Test105*"}
         user = self.client.post('/api/v1/login',
                                 user,
                                 content_type='application/json')
         user = user.json()
+        # Assign super admin role to the user
+        su_user = SystemUser.objects.filter(
+            email="admin@rush.com"
+        ).first()
+        su_user.user_access.role = UserRoleTypes.super_admin
+        su_user.user_access.save()
 
         form = Forms.objects.first()
         self.assertEqual(form.id, 1)
@@ -145,7 +152,7 @@ class FormDataUpdateTestCase(TestCase):
                 self.assertEqual(history[0]['value'], ['Male'])
                 self.assertEqual(history[0]['created_by'], 'Admin RUSH')
 
-    def test_update_datapoint_by_data_entry_role(self):
+    def test_update_datapoint_by_editor_access(self):
         form = Forms.objects.first()
         self.assertEqual(form.id, 1)
         self.assertEqual(form.name, "Test Form")
@@ -192,7 +199,7 @@ class FormDataUpdateTestCase(TestCase):
         self.assertEqual(data.status_code, 200)
         data = data.json()
         self.assertEqual(data, {"message": "ok"})
-        # create non admin user/data entry user
+        # create a new user
         payload = {
             "first_name": "User",
             "last_name": "Wayan",
@@ -217,10 +224,10 @@ class FormDataUpdateTestCase(TestCase):
                                **header)
         self.assertEqual(res.status_code, 200)
         self.assertEqual(res.json(), {'message': 'User added successfully'})
-        data_entry = SystemUser.objects.filter(
+        new_user = SystemUser.objects.filter(
             email="wayan@example.com").first()
         # check invitation
-        invite_payload = signing.dumps(data_entry.pk)
+        invite_payload = signing.dumps(new_user.pk)
         invite_response = self.client.get(
             '/api/v1/invitation/{0}'.format(invite_payload),
             content_type='application/json')
@@ -240,16 +247,20 @@ class FormDataUpdateTestCase(TestCase):
         call_command("demo_approval_flow", "--test", True)
 
         # data entry user login
-        data_entry_user = {"email": "wayan@example.com",
-                           "password": "Test123*"}
-        data_entry_user = self.client.post('/api/v1/login',
-                                           data_entry_user,
-                                           content_type='application/json')
-        data_entry_user = data_entry_user.json()
-        data_entry_user_token = data_entry_user.get('token')
-        self.assertTrue(data_entry_user_token)
+        new_user_user = {
+            "email": "wayan@example.com",
+            "password": "Test123*"
+        }
+        new_user_user = self.client.post(
+            '/api/v1/login',
+            new_user_user,
+            content_type='application/json',
+        )
+        new_user_user = new_user_user.json()
+        new_user_user_token = new_user_user.get('token')
+        self.assertTrue(new_user_user_token)
         # get profile
-        header = {'HTTP_AUTHORIZATION': f'Bearer {data_entry_user_token}'}
+        header = {'HTTP_AUTHORIZATION': f'Bearer {new_user_user_token}'}
         response = self.client.get("/api/v1/profile",
                                    content_type='application/json',
                                    **header)
@@ -261,7 +272,7 @@ class FormDataUpdateTestCase(TestCase):
         data = self.client.get('/api/v1/form-data/1?submission_type=1&page=1',
                                content_type='application/json',
                                **{'HTTP_AUTHORIZATION':
-                                   f'Bearer {data_entry_user_token}'})
+                                   f'Bearer {new_user_user_token}'})
         self.assertEqual(data.status_code, 200)
         data = data.json()
         self.assertEqual(len(data['data']) > 0, True)
@@ -270,7 +281,7 @@ class FormDataUpdateTestCase(TestCase):
         data = self.client.get(f'/api/v1/data/{data_id}',
                                content_type='application/json',
                                **{'HTTP_AUTHORIZATION':
-                                   f'Bearer {data_entry_user_token}'})
+                                   f'Bearer {new_user_user_token}'})
         self.assertEqual(data.status_code, 200)
         data = data.json()
         self.assertEqual(len(data) > 0, True)
@@ -298,49 +309,149 @@ class FormDataUpdateTestCase(TestCase):
                                payload,
                                content_type='application/json',
                                **{'HTTP_AUTHORIZATION':
-                                   f'Bearer {data_entry_user_token}'})
+                                   f'Bearer {new_user_user_token}'})
         self.assertEqual(data.status_code, 200)
         data = data.json()
-        self.assertEqual(data, {"message": "store to pending data success"})
-        # Get all data from pending data
-        data = self.client.get('/api/v1/form-pending-data/1?page=1',
-                               content_type='application/json',
-                               **{'HTTP_AUTHORIZATION':
-                                   f'Bearer {data_entry_user_token}'})
+        self.assertEqual(data, {"message": "direct update success"})
+
+    def test_update_datapoint_by_reader_access(self):
+        user = {
+            "email": "admin@rush.com",
+            "password": "Test105*"
+        }
+        user = self.client.post(
+            "/api/v1/login",
+            user,
+            content_type="application/json"
+        )
+        user = user.json()
+        token = user.get("token")
+        self.assertTrue(token)
+        # Add data to edit
+        payload = {
+            "data": {
+                "name": "Testing Data #3",
+                "administration": self.adm.id,
+                "geo": [6.2088, 106.8456]
+            },
+            "answer": [{
+                "question": 101,
+                "value": "John"
+            }, {
+                "question": 102,
+                "value": ["Male"]
+            }, {
+                "question": 103,
+                "value": 31208200175
+            }, {
+                "question": 104,
+                "value": 1
+            }, {
+                "question": 105,
+                "value": [6.2088, 106.8456]
+            }, {
+                "question": 106,
+                "value": ["Parent"]
+            }]
+        }
+        data = self.client.post(
+            "/api/v1/form-data/1/",
+            payload,
+            content_type="application/json",
+            **{"HTTP_AUTHORIZATION": f"Bearer {token}"}
+        )
+        self.assertEqual(data.status_code, 200)
+        data = data.json()
+        self.assertEqual(data, {"message": "ok"})
+        # create a new user
+        email = "user.reader@test.com"
+        payload = {
+            "first_name": "User",
+            "last_name": "Reader",
+            "email": email,
+            "administration": self.adm.id,
+            "access_forms": [
+                {
+                    "form_id": 1,
+                    "access_type": FormAccessTypes.read
+                },
+            ],
+            "role": UserRoleTypes.admin
+        }
+        header = {"HTTP_AUTHORIZATION": f"Bearer {token}"}
+        res = self.client.post(
+            "/api/v1/user",
+            payload,
+            content_type="application/json",
+            **header
+        )
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(res.json(), {"message": "User added successfully"})
+        new_user = SystemUser.objects.filter(
+            email=email).first()
+        # check invitation
+        invite_payload = signing.dumps(new_user.pk)
+        invite_response = self.client.get(
+            "/api/v1/invitation/{0}".format(invite_payload),
+            content_type="application/json")
+        self.assertEqual(invite_response.status_code, 200)
+        self.assertEqual(
+            invite_response.json(),
+            {"name": "User Reader"}
+        )
+        # set password
+        password_payload = {
+            "invite": invite_payload,
+            "password": "Test123*",
+            "confirm_password": "Test123*"
+        }
+        invite_response = self.client.put(
+            "/api/v1/user/set-password",
+            password_payload,
+            content_type="application/json"
+        )
+        self.assertEqual(invite_response.status_code, 200)
+
+        # Login as reader user
+        reader_user = {
+            "email": "user.reader@test.com",
+            "password": "Test123*"
+        }
+        reader_user = self.client.post(
+            "/api/v1/login",
+            reader_user,
+            content_type="application/json"
+        )
+        reader_user = reader_user.json()
+        reader_user_token = reader_user.get("token")
+        self.assertTrue(reader_user_token)
+
+        # Get all data from form
+        data = self.client.get(
+            '/api/v1/form-data/1?submission_type=1&page=1',
+            content_type='application/json',
+            **{'HTTP_AUTHORIZATION': f'Bearer {reader_user_token}'}
+        )
         self.assertEqual(data.status_code, 200)
         data = data.json()
         self.assertEqual(len(data['data']) > 0, True)
-        self.assertEqual(data['data'][0]['name'], 'Testing Data #2')
-        self.assertEqual(data['data'][0]['created_by'], "User Wayan")
-        self.assertEqual(data['data'][0]['data_id'], data_id)
-        pending_data_id = data['data'][0]['id']  # get pending_data_id here
-        # Get pending answer by pending_data_id
-        data = self.client.get(f'/api/v1/pending-data/{pending_data_id}',
-                               content_type='application/json',
-                               **{'HTTP_AUTHORIZATION':
-                                   f'Bearer {data_entry_user_token}'})
-        self.assertEqual(data.status_code, 200)
-        data = data.json()
-        self.assertEqual(len(data) > 0, True)
-        results = [{
-            'history': None,
-            'question': 101,
-            'value': 'User Wayan',
-            'last_value': None,
+        data_id = data['data'][0]['id']  # get data_id here
+        # Update data for question 101 and 104
+        payload = [{
+            "question": 101,
+            "value": "User Reader"
         }, {
-            'history': None,
-            'question': 102,
-            'value': ['Male'],
-            'last_value': None,
+            "question": 104,
+            "value": 999999
         }]
-        self.assertEqual(data, results)
-        # test get form data with pending data object inside
-        data = self.client.get('/api/v1/form-data/1?submission_type=1&page=1',
-                               content_type='application/json',
-                               **{'HTTP_AUTHORIZATION':
-                                   f'Bearer {token}'})
-        self.assertEqual(data.status_code, 200)
+        data = self.client.put(
+            f"/api/v1/form-data/1?data_id={data_id}",
+            payload,
+            content_type="application/json",
+            **{"HTTP_AUTHORIZATION": f"Bearer {reader_user_token}"}
+        )
+        self.assertEqual(data.status_code, 403)
         data = data.json()
-        self.assertEqual(len(data['data']) > 0, True)
-        self.assertEqual(data['data'][0]['pending_data'],
-                         {'id': pending_data_id, 'created_by': 'User Wayan'})
+        self.assertEqual(data, {
+            "detail": "You do not have permission to perform this action."
+        })
