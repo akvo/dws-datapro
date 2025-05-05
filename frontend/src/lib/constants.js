892c871df48d71a060c600df22b40b03f7b023cc
Iwan Firmawan <firmawaneiwan@gmail.com>
2025-05-05 15:23:05 +0700

    [#31] Add IMAGE_EXTENSIONS constant for supported image formats

--- frontend/src/lib/constants.js
+++ frontend/src/lib/constants.js
@@ -3,3 +3,16 @@ export const IS_ADMIN = 2;
 export const FORM_READER_ACCESS = 1;
 export const FORM_EDITOR_ACCESS = 2;
 export const FORM_APPROVER_ACCESS = 3;
+export const IMAGE_EXTENSIONS = [
+  "jpg",
+  "jpeg",
+  "png",
+  "gif",
+  "bmp",
+  "tiff",
+  "webp",
+  "heif",
+  "heic",
+  "svg",
+  "ico",
+];
