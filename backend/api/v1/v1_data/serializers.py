f1ce0cb5c52498772d717969defe7741a3bbd8ce
Iwan Firmawan <firmawaneiwan@gmail.com>
2025-05-07 15:16:16 +0700

    [#33] Add support for signature question type in Answers and serializers

--- backend/api/v1/v1_data/serializers.py
+++ backend/api/v1/v1_data/serializers.py
@@ -108,6 +108,7 @@ class SubmitFormDataAnswerSerializer(serializers.ModelSerializer):
             QuestionTypes.photo,
             QuestionTypes.date,
             QuestionTypes.attachment,
+            QuestionTypes.signature,
         ]:
             raise ValidationError(
                 "Valid string value is required for Question:{0}".format(
@@ -188,6 +189,7 @@ class SubmitFormSerializer(serializers.Serializer):
                 QuestionTypes.date,
                 QuestionTypes.autofield,
                 QuestionTypes.attachment,
+                QuestionTypes.signature,
             ]:
                 name = answer.get("value")
             elif answer.get("question").type == QuestionTypes.cascade:
@@ -1088,6 +1090,7 @@ class SubmitPendingFormDataAnswerSerializer(serializers.ModelSerializer):
             QuestionTypes.photo,
             QuestionTypes.date,
             QuestionTypes.attachment,
+            QuestionTypes.signature,
         ]:
             raise ValidationError(
                 f"Valid string value is required for Question: {question.id}"
@@ -1190,6 +1193,7 @@ class SubmitPendingFormSerializer(serializers.Serializer):
                 QuestionTypes.date,
                 QuestionTypes.autofield,
                 QuestionTypes.attachment,
+                QuestionTypes.signature,
             ]:
                 name = answer.get("value")
             elif question.type == QuestionTypes.cascade:
