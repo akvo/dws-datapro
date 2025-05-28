39d82d0d56135ddbb997135fc3be2bf9e79d15cd
Iwan Firmawan <firmawaneiwan@gmail.com>
2025-05-28 10:35:38 +0700

    [#48] v1_data: Refactor seed_approved_data function to streamline form data creation and remove unnecessary update logic

--- backend/api/v1/v1_data/tasks.py
+++ backend/api/v1/v1_data/tasks.py
@@ -1,9 +1,7 @@
-from django.utils import timezone
 from api.v1.v1_data.models import (
     FormData,
     Answers,
     PendingAnswers,
-    AnswerHistory,
 )
 
 
@@ -13,47 +11,19 @@ def seed_approved_data(data):
         uuid=data.uuid,
         parent__isnull=True,
     ).first()
-    # only registration data will be updated
-    if data.data:
-        form_data: FormData = data.data
-        form_data.parent = parent_data
-        form_data.name = data.name
-        form_data.uuid = data.uuid
-        form_data.form = data.form
-        form_data.administration = data.administration
-        form_data.geo = data.geo
-        form_data.updated_by = data.created_by
-        form_data.updated = timezone.now()
-        form_data.save()
-
-        for answer in data.pending_data_answer.all():
-            form_answer = Answers.objects.get(
-                data=form_data, question=answer.question
-            )
-
-            AnswerHistory.objects.create(
-                data=form_answer.data,
-                question=form_answer.question,
-                name=form_answer.name,
-                value=form_answer.value,
-                options=form_answer.options,
-                created_by=form_answer.created_by,
-            )
-            form_answer.delete()
-    else:
-        form_data = FormData.objects.create(
-            parent=parent_data,
-            name=data.name,
-            uuid=data.uuid,
-            form=data.form,
-            administration=data.administration,
-            geo=data.geo,
-            created_by=data.created_by,
-            created=data.created,
-        )
-        data.data = form_data
-        data.approved = True
-        data.save()
+    form_data = FormData.objects.create(
+        parent=parent_data,
+        name=data.name,
+        uuid=data.uuid,
+        form=data.form,
+        administration=data.administration,
+        geo=data.geo,
+        created_by=data.created_by,
+        created=data.created,
+    )
+    data.data = form_data
+    data.approved = True
+    data.save()
 
     answer: PendingAnswers
     for answer in data.pending_data_answer.all():
