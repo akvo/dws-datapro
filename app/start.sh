6ed275b575cfffa4a89b15758e72c631b456e80f
Iwan Firmawan <firmawaneiwan@gmail.com>
2025-05-30 05:45:52 +0700

    [#50] Refactor startup scripts: Consolidate APP_NAME handling and remove obsolete update script

--- app/start.sh
+++ app/start.sh
@@ -9,5 +9,8 @@ echo "SENTRY_DSN=${SENTRY_DSN}" >>.env
 echo "SENTRY_AUTH_TOKEN=${SENTRY_AUTH_TOKEN}" >>.env
 echo "APP_NAME=${APP_NAME}" >>.env
 echo "APP_SHORT_NAME=${APP_SHORT_NAME}" >>.env
+
+sed -i "s|\"name\": \".*\"|\"name\": \"${APP_NAME}\"|" app.json
+
 yarn install
 yarn start
