0785286904755822874c2899c10612698a2d305a
Iwan Firmawan <firmawaneiwan@gmail.com>
2025-05-29 15:28:05 +0700

    [#48] v1_data: Refactor seed_approved_data to update existing FormData instead of creating new instances

--- backend/api/v1/v1_data/tasks.py
+++ backend/api/v1/v1_data/tasks.py
@@ -1,39 +1,17 @@
-from api.v1.v1_data.models import (
-    FormData,
-    Answers,
-    PendingAnswers,
-)
+from django.utils import timezone
+from api.v1.v1_data.models import FormData
 
 
-def seed_approved_data(data):
-    parent_data = FormData.objects.filter(
-        form=data.form,
-        uuid=data.uuid,
-        parent__isnull=True,
-    ).first()
-    form_data = FormData.objects.create(
-        parent=parent_data,
-        name=data.name,
-        uuid=data.uuid,
-        form=data.form,
-        administration=data.administration,
-        geo=data.geo,
-        created_by=data.created_by,
-        created=data.created,
-    )
-    data.data = form_data
-    data.approved = True
+def seed_approved_data(data: FormData):
+    """
+    Update FormData object from pending status to approved status
+    """
+    # No need to create new data, just update existing FormData
+    data.updated = timezone.now()
+    data.is_pending = False
     data.save()
 
-    answer: PendingAnswers
-    for answer in data.pending_data_answer.all():
-        Answers.objects.create(
-            data=form_data,
-            question=answer.question,
-            name=answer.name,
-            value=answer.value,
-            options=answer.options,
-            created_by=answer.created_by,
-        )
+    # Save to file after approval
+    data.save_to_file
 
-    form_data.save_to_file
+    return data
