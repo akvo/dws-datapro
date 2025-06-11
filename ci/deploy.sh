c738e1e60b47d452bc30053b4ea559dd9adec634
Iwan Firmawan <firmawaneiwan@gmail.com>
2025-06-11 17:37:18 +0700

    [#58] fix: include APP_SHORT_NAME substitution in deployment preparation script

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -34,7 +34,7 @@ prepare_deployment () {
 
     gcloud container clusters get-credentials "${cluster}"
 
-    sed -e "s/\${CI_COMMIT}/${CI_COMMIT}/g;" \
+    sed -e "s/\${CI_COMMIT}/${CI_COMMIT}/g;" -e "s/\${APP_SHORT_NAME}/${APP_SHORT_NAME}/g;" \
         ci/k8s/deployment.template.yml > ci/k8s/deployment.yml
 
     sed -e "s/\${CI_COMMIT}/${CI_COMMIT}/g;" \
