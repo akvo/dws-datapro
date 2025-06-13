ecd8e768f3ed578c5e9abf82ff41eea2008a593f
Iwan Firmawan <firmawaneiwan@gmail.com>
2025-06-13 14:16:06 +0700

    [#58] Mobile app: Refactor environment variable usage to replace APK_DOMAIN with APK_SHORT_NAME in start.sh

--- app/start.sh
+++ app/start.sh
@@ -8,14 +8,14 @@ echo "SENTRY_ENV=${SENTRY_ENV}" >>.env
 echo "SENTRY_DSN=${SENTRY_DSN}" >>.env
 echo "SENTRY_AUTH_TOKEN=${SENTRY_AUTH_TOKEN}" >>.env
 echo "APK_NAME=${APK_NAME}" >>.env
-echo "APK_DOMAIN=${APK_DOMAIN}" >>.env
+echo "APK_SHORT_NAME=${APK_SHORT_NAME}" >>.env
 
 sed -i "s|\"name\": \".*\"|\"name\": \"${APK_NAME}\"|" app.json
-sed -i "s|\"slug\": \".*\"|\"slug\": \"${APK_DOMAIN}-mobile\"|" app.json
+sed -i "s|\"slug\": \".*\"|\"slug\": \"${APK_SHORT_NAME}-mobile\"|" app.json
 # Convert any hyphens (-) to underscores (_) for Android package name
-ANDROID_PACKAGE=$(echo ${APK_DOMAIN} | sed 's/-/_/g')
+ANDROID_PACKAGE=$(echo ${APK_SHORT_NAME} | sed 's/-/_/g')
 sed -i "s|\"package\": \"com\.akvo\..*\"|\"package\": \"com.akvo.${ANDROID_PACKAGE}\"|" app.json
-sed -i "s|\"name\": \".*\"|\"name\": \"${APK_DOMAIN}-mobile\"|" package.json
+sed -i "s|\"name\": \".*\"|\"name\": \"${APK_SHORT_NAME}-mobile\"|" package.json
 
 yarn install
 yarn start
