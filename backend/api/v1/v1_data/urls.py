6a954d21927f3aee07d46a2c2fa19bef4bf42773
Iwan Firmawan <firmawaneiwan@gmail.com>
2025-06-18 09:52:09 +0700

    [#64] v1_data: Remove related approval workflow in views and update the urls.py

--- backend/api/v1/v1_data/urls.py
+++ backend/api/v1/v1_data/urls.py
@@ -2,16 +2,10 @@ from django.urls import re_path
 
 from api.v1.v1_data.views import (
     DataAnswerDetailDeleteView,
-    approve_pending_data,
     export_form_data,
-    list_pending_batch,
-    list_pending_data_batch,
     FormDataAddListView,
     PendingFormDataView,
-    BatchView,
     PendingDataDetailDeleteView,
-    BatchSummaryView,
-    BatchCommentView,
     DataDetailDeleteView,
 )
 from api.v1.v1_users.views import health_check, get_config_file, email_template
@@ -29,11 +23,6 @@ urlpatterns = [
         r"^(?P<version>(v1))/form-pending-data/(?P<form_id>[0-9]+)",
         PendingFormDataView.as_view(),
     ),
-    re_path(r"^(?P<version>(v1))/form-pending-batch", list_pending_batch),
-    re_path(
-        r"^(?P<version>(v1))/form-pending-data-batch/(?P<batch_id>[0-9]+)",
-        list_pending_data_batch,
-    ),
     re_path(
         r"^(?P<version>(v1))/data-details/(?P<data_id>[0-9]+)",
         DataDetailDeleteView.as_view(),
@@ -42,16 +31,6 @@ urlpatterns = [
         r"^(?P<version>(v1))/pending-data/(?P<pending_data_id>[0-9]+)",
         PendingDataDetailDeleteView.as_view(),
     ),
-    re_path(r"^(?P<version>(v1))/pending-data/approve", approve_pending_data),
-    re_path(
-        r"^(?P<version>(v1))/batch/comment/(?P<batch_id>[0-9]+)",
-        BatchCommentView.as_view(),
-    ),
-    re_path(
-        r"^(?P<version>(v1))/batch/summary/(?P<batch_id>[0-9]+)",
-        BatchSummaryView.as_view(),
-    ),
-    re_path(r"^(?P<version>(v1))/batch", BatchView.as_view()),
     re_path(
         r"^(?P<version>(v1))/export/form/(?P<form_id>[0-9]+)", export_form_data
     ),
