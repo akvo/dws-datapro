09ae87af8449dc804179955cbe47027afc2e5160
Iwan Firmawan <firmawaneiwan@gmail.com>
2025-06-19 11:16:40 +0700

    [#64] FE: Create AddRolePage component for adding new roles

---
+++ frontend/src/pages/add-role/AddRole.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AddRolePage = () => {
+  return (
+    <div>
+      <h1>Add Role Page</h1>
+      <p>This is the page for adding a new role.</p>
+    </div>
+  );
+};
+
+export default AddRolePage;
