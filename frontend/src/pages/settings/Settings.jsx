79609b6f4ad94defba9b47d1db27e68de1985d13
Iwan Firmawan <firmawaneiwan@gmail.com>
2025-06-20 14:07:42 +0700

    [#64] FE: Refactor Settings component to remove unused code and simplify panel rendering

--- frontend/src/pages/settings/Settings.jsx
+++ frontend/src/pages/settings/Settings.jsx
@@ -2,32 +2,17 @@
 import React, { useMemo } from "react";
 import "./style.scss";
 import { Row, Col, Card, Button, Divider } from "antd";
-import { store, config, uiText } from "../../lib";
+import { store, uiText } from "../../lib";
 import { Link } from "react-router-dom";
 import { Breadcrumbs, DescriptionPanel } from "../../components";
+import { Can } from "../../components/can";
 
 const Settings = () => {
-  const { user: authUser } = store.useState((s) => s);
   const { language } = store.useState((s) => s);
   const { active: activeLang } = language;
   const text = useMemo(() => {
     return uiText[activeLang];
   }, [activeLang]);
-
-  const panels = [
-    {
-      title: text.orgPanelTitle,
-      buttonLabel: text.orgPanelButton,
-      access: "organisation",
-      description: <div>{text.orgPanelText}</div>,
-      link: "/control-center/master-data/organisations",
-      image: "/assets/personal-information.png",
-    },
-  ];
-
-  const selectedPanels = panels.filter((p) =>
-    config.checkAccess(authUser?.roles, p.access)
-  );
 
   return (
     <div id="settings">
@@ -47,26 +32,33 @@ const Settings = () => {
       />
       <Divider />
       <Row gutter={[16, 16]}>
-        {selectedPanels.map((panel, index) => (
-          <Col className="card-wrapper" span={12} key={index}>
+        <Can I="manage" a="master-data">
+          <Col className="card-wrapper" span={12}>
             <Card bordered={false} hoverable>
               <div className="row">
                 <div className="flex-1">
-                  <h2>{panel.title}</h2>
-                  <span>{panel.description}</span>
-                  <Link to={panel.link} className="explore">
+                  <h2>{text.orgPanelTitle}</h2>
+                  <span>{text.orgPanelDescription}</span>
+                  <Link
+                    to="/control-center/master-data/organisations"
+                    className="explore"
+                  >
                     <Button type="primary" shape="round">
-                      {panel.buttonLabel}
+                      {text.orgPanelButton}
                     </Button>
                   </Link>
                 </div>
                 <div>
-                  <img src={panel.image} width={100} height={100} />
+                  <img
+                    src="/assets/personal-information.png"
+                    width={100}
+                    height={100}
+                  />
                 </div>
               </div>
             </Card>
           </Col>
-        ))}
+        </Can>
       </Row>
     </div>
   );
