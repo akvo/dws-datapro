df56aaaa3d19556494479b8c10a8dd4466e146de
Iwan Firmawan <firmawaneiwan@gmail.com>
2025-06-20 14:06:52 +0700

    [#64] FE: Refactor Sidebar component to implement role-based access control and improve menu structure

--- frontend/src/components/sidebar/index.jsx
+++ frontend/src/components/sidebar/index.jsx
@@ -1,6 +1,7 @@
-import React, { useCallback, useMemo, useState } from "react";
+import React, { useContext, useMemo } from "react";
 import { Layout, Menu } from "antd";
-import { store, config, api, IS_SUPER_ADMIN } from "../../lib";
+import { store, uiText } from "../../lib";
+import api from "../../lib/api";
 import { useNavigate } from "react-router-dom";
 import {
   UserOutlined,
@@ -9,178 +10,20 @@ import {
   DashboardOutlined,
   DownloadOutlined,
 } from "@ant-design/icons";
+import { AbilityContext, Can } from "../can";
 
 const { Sider } = Layout;
 
 const Sidebar = () => {
-  const { user: authUser, administration } = store.useState((s) => s);
-  const [selectedKey, setSelectedKey] = useState("");
-  const [openKeys, setOpenKeys] = useState([]);
+  const { user: authUser, administration, language } = store.useState((s) => s);
   const navigate = useNavigate();
 
-  const { roles, checkApproverAccess } = config;
-  // TODO: Implement RBAC
-  const superAdminRole = roles.find((r) => r.id === IS_SUPER_ADMIN);
-  const isApprover = checkApproverAccess(authUser?.forms);
+  const { active: activeLang } = language || { active: "en" };
+  const text = useMemo(() => {
+    return uiText[activeLang] || uiText.en;
+  }, [activeLang]);
 
-  const pageAccessToLabelAndUrlMapping = {
-    user: { label: "Manage Platform Users", url: "/control-center/users" },
-    approvers: {
-      label: "Validation Tree",
-      url: "/control-center/approvers/tree",
-    },
-    mobile: {
-      label: "Manage Mobile Users",
-      url: "/control-center/mobile-assignment",
-    },
-    data: { label: "Manage Data", url: "/control-center/data" },
-    "master-data": [
-      {
-        label: "Administrative List",
-        url: "/control-center/master-data/administration",
-      },
-      { label: "Attributes", url: "/control-center/master-data/attributes" },
-      { label: "Entities", url: "/control-center/master-data/entities" },
-      {
-        label: "Entity Types",
-        url: "/control-center/master-data/entity-types",
-      },
-      {
-        label: "Organisations",
-        url: "/control-center/master-data/organisations",
-      },
-    ],
-    submissions: {
-      label: "Manage Submissions",
-      url: "/control-center/data/submissions",
-    },
-    approvals: {
-      label: "Manage Approvals",
-      url: "/control-center/approvals",
-    },
-    roles: {
-      label: "Manage Roles",
-      url: "/control-center/roles",
-    },
-  };
-
-  const controlCenterToLabelMapping = useMemo(() => {
-    const manageDataChildren = ["data", "submissions"];
-    if (isApprover) {
-      manageDataChildren.push("approvals");
-    }
-    const mapping = {
-      "control-center": {
-        label: "Control Center",
-        icon: DashboardOutlined,
-      },
-      "manage-user": {
-        label: "Users",
-        icon: UserOutlined,
-        childrenKeys: ["user", "approvers", "mobile", "roles"],
-      },
-      "manage-data": {
-        label: "Data",
-        icon: TableOutlined,
-        childrenKeys: manageDataChildren,
-      },
-      "manage-master-data": {
-        label: "Master Data",
-        icon: DatabaseOutlined,
-        childrenKeys: ["master-data"],
-      },
-      download: {
-        label: "Downloads",
-        icon: DownloadOutlined,
-      },
-    };
-    return mapping;
-  }, [isApprover]);
-
-  const determineChildren = (key) => {
-    const mapping = pageAccessToLabelAndUrlMapping[key];
-    if (Array.isArray(mapping)) {
-      return mapping.map((item, index) => ({
-        key: key + "_" + index,
-        ...item,
-      }));
-    }
-    return [{ key, ...mapping }];
-  };
-
-  const createMenuItems = (controlCenterOrder, pageAccess) => {
-    const menuItems = [];
-    const controlCenterItem = controlCenterToLabelMapping["control-center"];
-    const downloadItem = controlCenterToLabelMapping["download"];
-    if (controlCenterItem) {
-      menuItems.push({
-        key: "control-center",
-        icon: controlCenterItem.icon
-          ? React.createElement(controlCenterItem.icon)
-          : null,
-        label: controlCenterItem.label,
-        url: "/control-center",
-      });
-    }
-    Object.keys(controlCenterToLabelMapping).forEach((orderKey) => {
-      if (orderKey === "control-center" || orderKey === "download") {
-        return;
-      }
-
-      const item = controlCenterToLabelMapping[orderKey];
-      if (!item) {
-        return;
-      }
-
-      const { label, icon, childrenKeys } = item;
-
-      const shouldIncludeItem =
-        controlCenterOrder.includes(orderKey) ||
-        childrenKeys.some((childKey) => pageAccess.includes(childKey));
-
-      if (shouldIncludeItem) {
-        const children = childrenKeys
-          .filter((key) => pageAccess.includes(key.split(/(\d+)/)[0]))
-          .flatMap((key) => determineChildren(key, pageAccess));
-
-        menuItems.push({
-          key: orderKey,
-          icon: icon ? React.createElement(icon) : null,
-          label,
-          children: children.length ? children : null,
-        });
-      }
-    });
-    if (downloadItem) {
-      menuItems.push({
-        key: "download",
-        icon: downloadItem.icon ? React.createElement(downloadItem.icon) : null,
-        label: downloadItem.label,
-        url: "/downloads",
-      });
-    }
-
-    return menuItems;
-  };
-
-  const usersMenuItem = createMenuItems(
-    superAdminRole.control_center_order,
-    superAdminRole.page_access
-  );
-
-  const findUrlByKey = (items, key) => {
-    for (const item of items) {
-      if (item.key === key) {
-        return item.url;
-      }
-      if (item.children) {
-        const url = findUrlByKey(item.children, key);
-        if (url) {
-          return url;
-        }
-      }
-    }
-  };
+  const ability = useContext(AbilityContext);
 
   const handleResetGlobalFilterState = async () => {
     // reset global filter store when moving page on sidebar click
@@ -208,53 +51,151 @@ const Sidebar = () => {
     }
   };
 
-  const handleMenuClick = ({ key }) => {
-    setSelectedKey(key);
+  const handleMenuClick = ({ item }) => {
+    // Get the URL from the menu item
+    const url = item.props?.["data-url"];
+    // Reset global filter state
     handleResetGlobalFilterState();
-    const url = findUrlByKey(usersMenuItem, key);
+    // Navigate to the URL
     navigate(url);
   };
-
-  const findKeyByUrl = useCallback((items, url) => {
-    for (const item of items) {
-      if (item.url === url) {
-        return item.key;
-      }
-      if (url.split("/").length > 2) {
-        const parentUrl = url.split("/").slice(0, 3).join("/");
-        if (parentUrl === item.url) {
-          return item.key;
-        }
-      }
-      if (url.split("/").length > 3) {
-        const parentUrl = url.split("/").slice(0, 4).join("/");
-        if (parentUrl === item.url) {
-          return item.key;
-        }
-      }
-      if (item.children) {
-        const key = findKeyByUrl(item.children, url);
-        if (key) {
-          return key;
-        }
-      }
-    }
-  }, []);
 
   return (
     <Sider className="site-layout-background">
       <Menu
         mode="inline"
-        selectedKeys={[selectedKey]}
-        openKeys={openKeys}
         style={{
           height: "100%",
           borderRight: 0,
         }}
-        onSelect={handleMenuClick}
-        onOpenChange={(newOpenKeys) => setOpenKeys(newOpenKeys)}
-        items={usersMenuItem}
-      />
+        onClick={handleMenuClick}
+      >
+        {/* Control Center */}
+
+        <Menu.Item
+          key="menu-control-center"
+          icon={<DashboardOutlined />}
+          data-url="/control-center"
+        >
+          {text.menuControlCenter}
+        </Menu.Item>
+
+        {/* Users SubMenu */}
+        <Menu.SubMenu
+          key="manage-user"
+          icon={<UserOutlined />}
+          title={text.menuUsers}
+        >
+          {/* Wrap each menu item explicitly with a unique key for Can */}
+          <Can I="manage" a="user" key="can-platform-users">
+            <Menu.Item
+              key="menu-platform-users"
+              data-url="/control-center/users"
+            >
+              {text.menuManagePlatformUsers}
+            </Menu.Item>
+          </Can>
+          <Menu.Item
+            key="menu-approvers"
+            data-url="/control-center/approvers/tree"
+          >
+            {text.menuValidationTree}
+          </Menu.Item>
+          {ability.can("read", "mobile") && (
+            <Menu.Item
+              key="menu-mobile-users"
+              data-url="/control-center/mobile-assignment"
+            >
+              {text.menuManageMobileUsers}
+            </Menu.Item>
+          )}
+          {ability.can("manage", "roles") && (
+            <Menu.Item key="menu-roles" data-url="/control-center/roles">
+              {text.menuManageRoles}
+            </Menu.Item>
+          )}
+        </Menu.SubMenu>
+
+        {/* Data SubMenu */}
+        <Menu.SubMenu
+          key="manage-data"
+          icon={<TableOutlined />}
+          title={text.menuData}
+        >
+          <Menu.Item key="menu-manage-data" data-url="/control-center/data">
+            {text.menuManageData}
+          </Menu.Item>
+
+          {ability.can("manage", "submissions") && (
+            <Menu.Item
+              key="menu-submissions"
+              data-url="/control-center/data/submissions"
+            >
+              {text.menuPendingSubmissions}
+            </Menu.Item>
+          )}
+
+          {ability.can("manage", "approvals") && (
+            <Menu.Item
+              key="menu-approvals"
+              data-url="/control-center/approvals"
+            >
+              {text.menuApprovals}
+            </Menu.Item>
+          )}
+        </Menu.SubMenu>
+
+        {/* Master Data SubMenu */}
+        <Can I="manage" a="master-data" key="can-master-data">
+          <Menu.SubMenu
+            key="manage-master-data"
+            icon={<DatabaseOutlined />}
+            title={text.menuMasterData}
+          >
+            <Menu.Item
+              key="menu-master-data-administration"
+              data-url="/control-center/master-data/administration"
+            >
+              {text.menuAdministrativeList}
+            </Menu.Item>
+            <Menu.Item
+              key="menu-master-data-attributes"
+              data-url="/control-center/master-data/attributes"
+            >
+              {text.menuAttributes}
+            </Menu.Item>
+            <Menu.Item
+              key="menu-master-data-entities"
+              data-url="/control-center/master-data/entities"
+            >
+              {text.menuEntities}
+            </Menu.Item>
+            <Menu.Item
+              key="menu-master-data-entity-types"
+              data-url="/control-center/master-data/entity-types"
+            >
+              {text.menuEntityTypes}
+            </Menu.Item>
+            <Menu.Item
+              key="menu-master-data-organisations"
+              data-url="/control-center/master-data/organisations"
+            >
+              {text.menuOrganisations}
+            </Menu.Item>
+          </Menu.SubMenu>
+        </Can>
+
+        {/* Downloads */}
+        {ability.can("read", "downloads") && (
+          <Menu.Item
+            key="menu-downloads"
+            icon={<DownloadOutlined />}
+            data-url="/downloads"
+          >
+            {text.menuDownloads}
+          </Menu.Item>
+        )}
+      </Menu>
     </Sider>
   );
 };
