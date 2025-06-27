24462fcc11766e0dc980310b1ab0abb0f7e5cb66
Iwan Firmawan <firmawaneiwan@gmail.com>
2025-06-27 13:44:04 +0700

    [#74] Mobile app: Refactor build parameters and update APK name handling in build.json and start.sh

--- app/src/build.js
+++ app/src/build.js
@@ -1,12 +1,11 @@
 // eslint-disable-next-line import/no-unresolved
-import { SERVER_URL, APK_URL, APK_NAME } from '@env';
+import { SERVER_URL, APK_URL } from '@env';
 import buildJson from './build.json';
 
 const defaultBuildParams = {
   ...buildJson,
   serverURL: SERVER_URL,
   apkURL: APK_URL,
-  apkName: APK_NAME,
 };
 
 export default defaultBuildParams;
